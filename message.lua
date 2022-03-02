034c0b87fbf70e3a8b89af6eca34767365497059
Shane Celis <shane.celis@gmail.com>
2022-03-02 00:00:05 -0500

    refactor: Move message impl to .lua file.

---
+++ message.lua
@@ -0,0 +1,313 @@
+
+--https://www.lexaloffle.com/bbs/?tid=33645
+--shooting's ultimate text
+--[[
+    text codes:
+
+   $u1 = underline text (0 for
+           no underline)
+
+   $b## = border color, ##= a
+          number, 0-15
+
+   $o## = outline color
+
+   $c## = text color
+
+   $d## = delay extra (0-99)
+          if more delay is
+          needed, use $f##
+          and create a custom
+          fx for it.
+   $i## = display sprite inline
+
+   $f## = special effects
+
+   for any of these, you can use
+   xx instead of a number to
+   reset it to default (based
+   on the default config you
+   have set up)
+
+   alternatively, you can use
+   16 to set it to nil and
+   remove it.
+]]--
+--[[
+todo
+====
+* remove skip elements
+* generalize fragment to hold whole string not just one character
+* generalize framework so that all outline, highlight, and such are just effects
+
+changes
+=======
+* use's lua's oo system instead of a 'msg_' prefix
+* draw(x,y) lines up with print(s,x,y)
+* fragment class only hold items changed from default
+* parsing happens once
+* many messages may be used at the same time
+* buttons and timing are handled in _update() instead of _draw()
+*
+
+motivation
+==========
+
+shooting's ultimate text library is awesome first of all, so why change it? there were a few
+reasons: i wanted to integrate it with my code that has a particular style.
+
+]]--
+--==configurations==--
+--[[
+  configure your defaults
+  here
+--]]
+message = {
+  color = {
+    foreground = 15,
+    highlight = nil,
+    outline = 1
+  },
+  spacing = {
+    letter = 4,
+    newline = 7
+  },
+  sound = {
+    blip = 0,
+    next_message = 1
+  },
+  next_message = {
+    button = 5,
+    char = '.',
+    color = 9
+  },
+  effects = {
+    function(fragment, fxv)
+      local t = 1.5 * time()
+      fragment.dy=sin(t+fxv)
+    end,
+    function(fragment, fxv)
+      local t = 1.5 * time()
+      fragment.dy=sin(t+fxv)
+      fragment.dx = rnd(4) - 2
+    end
+  },
+  delay = 1/30,
+}
+
+function message:new(o)
+  o = o or {}
+  setmetatable(o, self)
+  if (o.color) setmetatable(o.color, self.color); self.color.__index = self.color
+  if (o.spacing) setmetatable(o.spacing, self.spacing); self.spacing.__index = self.spacing
+  if (o.sound) setmetatable(o.sound, self.sound); self.sound.__index = self.sound
+  if (o.next_message) setmetatable(o.next_message, self.next_message); self.next_message.__index = self.next_message
+  self.__index = self
+  o.fragments = {}
+  for k,v in ipairs(o) do
+    -- add(o.fragments, o:split(v))
+    add(o.fragments, o:parse(v))
+  end
+  o.istart = nil -- when we started displaying the ith message
+  o.i = 1 -- where we our in our
+  o.cur = 1 -- current string
+  return o
+end
+
+fragment = {
+  color = message.color,
+  c = nil,
+  dx = 0,
+  dy = 0,
+  fxv = 0,
+  delay = nil,
+  image = nil,
+  underline = nil,
+  delay_accum = 0
+}
+
+function fragment.new(class, o)
+  o = o or {}
+  if (o.color) setmetatable(o.color, message.color); message.color.__index = message.color
+  setmetatable(o, class)
+  class.__index = class
+  return o
+end
+
+function fragment:update()
+end
+
+--parse entire message :u
+function message:parse(string)
+  chars = {}
+  for i=1,#string do
+    add(chars, { c=sub(string, i, i), action = nil, skip = false, fragment_index = nil })
+  end
+  for i=1,#chars - 1 do
+    local t=chars[i].c
+    local c=chars[i+1].c
+    if t=='$' and (c=='c' or c=='b' or c=='f' or c=='d' or c=='o' or c=='i') then
+      chars[i].skip=true
+      chars[i+1].skip=true
+      chars[i+2].skip=true
+      chars[i+3].skip=true
+      local val=tonum(chars[i+2].c..chars[i+3].c)
+      chars[i+3].action = function(fragments, k)
+        if c == 'i' then
+          fragments[k].image=val
+        else
+          for j=k,#fragments do
+            if c=='c' then
+              fragments[j].color.foreground=val
+            elseif c=='b' then
+              fragments[j].color.highlight=val
+            elseif c=='f' then
+              fragments[j].update=self.effects[val]
+            elseif c=='d' then
+              -- delay is in terms of frames (could be 60 though &shrug;)
+              local t = val
+              if (t) t /= 30
+              fragments[j].delay=t
+            elseif c=='o' then
+              fragments[j].color.outline=val
+            end
+          end
+        end
+      end
+    elseif t == '$' and c == '$' then
+      -- $$ becomes $
+      chars[i+1].skip = true
+    end
+
+    if t=='$' and c=='u' then
+      chars[i].skip=true
+      chars[i+1].skip=true
+      chars[i+2].skip=true
+
+      local val = tonum(chars[i+2].c)
+      chars[i+2].action = function(fragments, k)
+        for j=k,#fragments do
+          fragments[j].underline=val
+        end
+      end
+    end
+  end
+  local fragments = {}
+  for char in all(chars) do
+    if not char.skip then
+      add(fragments, fragment:new { color = {}, c = char.c })
+    end
+    char.fragment_index = #fragments
+  end
+  for char in all(chars) do
+    if char.action then
+      char.action(fragments, char.fragment_index + 1)
+    end
+  end
+
+  local accum = 0
+  for k,f in ipairs(fragments) do
+    if not f.skip then
+      accum += f.delay or self.delay
+      f.delay_accum = accum
+    end
+  end
+  return fragments
+end
+
+function message:is_complete()
+  return self.cur > #self.fragments
+end
+
+function message:update()
+  if (not self.istart) self.istart = time()
+  local fragments = self.fragments[self.cur]
+  if (not fragments) return
+  if btnp(self.next_message.button) then
+    if self.i < #fragments then
+      self.i=#fragments
+      return
+    else
+      sfx(self.sound.next_message)
+      self.cur += 1
+      self.i = 1
+      self.istart = time()
+    end
+  end
+
+  if (self.i > #fragments) return
+  --like seriously, its just
+  --vital function stuff.
+  local delay = self.delay
+  -- if (self.i <= #fragments) delay += fragments[self.i].delay
+
+  if time() - self.istart > fragments[self.i].delay_accum then
+    self.i+=1
+    if (self.i <= #fragments) sfx(self.sound.blip)
+  end
+
+end
+
+function message:draw(x, y)
+  local fragments = self.fragments[self.cur]
+  if (not fragments) return
+  --loop...
+  --i mean, hey... if you want
+  --to keep reading, go ahead.
+  local _x=0
+  local _y=0
+  for i = 1, self.i do
+    if not fragments[i] then break end
+    --i wont try and stop you.
+    -- local str = sub(fragments[i].c, 1, self.i)
+    local str = fragments[i].c
+    local highlight = fragments[i].color.highlight
+    if highlight and highlight ~= 16 then
+      rectfill(x+_x-1, y+_y-1, x+_x+self.spacing.letter-1,y+_y+5, highlight)
+    end
+
+    if fragments[i].image then
+      spr(fragments[i].image, x+_x+fragments[i].dx, y+fragments[i].dy+_y)
+    end
+    --you're probably getting
+    --bored now, right?
+    local outline = fragments[i].color.outline
+    if outline and outline ~= 16 then
+      local __x=x+_x+fragments[i].dx
+      local __y=y+_y+fragments[i].dy
+      for i4=1,3 do
+        for j4=1,3 do
+          print(str, __x-2+i4, __y-2+j4, outline)
+        end
+      end
+    end
+
+    --yep, not much here...
+    print(str, x+_x+fragments[i].dx, y+fragments[i].dy+_y, fragments[i].color.foreground)
+    if fragments[i].underline == 1 then
+      line(x+_x, y+_y+5, x+_x+self.spacing.letter, y+_y+5)
+    end
+
+    _x+=self.spacing.letter
+    -- split by the newlines too?
+    if fragments[i].c == '\n' then
+      _x=0
+      _y+=self.spacing.newline
+    end
+  end
+
+  -- this is the dot
+  if self.i>=#fragments then
+    -- local _t = -0.05 * self.t
+    -- local _t = 1.5 * time()
+    local _t = 1.6 * time()
+    print(self.next_message.char, x+_x+cos(_t), y+_y+sin(_t), self.next_message.color)
+  end
+  --i mean, its not like
+  --i care.
+  for i=1,#fragments do
+    fragments[i]:update(i/3)
+  end
+
+  --enjoy the script :)--
+end
